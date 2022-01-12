5824820f961445485c72bdcc24fd407165bc642e
Edgar Chang <melikechoco@gmail.com>
2022-01-11 19:55:12 -0500

    hopefully this fixed email

--- src/components/about-me/about-me.component.ts
+++ src/components/about-me/about-me.component.ts
@@ -16,42 +16,40 @@ export class AboutMeComponent implements OnInit {
 
     // setTimeout(() => this.email = atob(atob('YldGcGJIUnZPbTFsYkdsclpXTm9iMk52UUdkdFlXbHNMbU52YlE9PQ==')), 1000);
 
-    // setTimeout(() => {
-
-    //   // Email obfuscator script 2.1 by Tim Williams, University of Arizona
-    //   // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
-    //   // This code is freeware provided these four comment lines remain intact
-    //   // A wizard to generate this code is at http://www.jottings.com/obfuscator/
-
-    //   const coded = "RSFciSPHbPb@5RVcF.PbR"
-    //   const key = "MmjstTqyJkPg2d409eAFCuRoa67hXLiO5xwZnYSpr8G3bVWvH1zcNIEfUKBDlQ"
-    //   const shift = coded.length
-    //   let link = ""
-
-    //   for (let i = 0; i < coded.length; i++) {
-
-    //     if (key.indexOf(coded.charAt(i)) == -1) {
-
-    //       const ltr = coded.charAt(i)
-    //       link += (ltr)
-
-    //     }
-    //     else {
-
-    //       const ltr = (key.indexOf(coded.charAt(i)) - shift + key.length) % key.length
-    //       link += (key.charAt(ltr))
-
-    //     }
-
-    //   }
-
-    //   this.email = 'mailto:' + link;
-
-    // }, 1000);
-
     setTimeout(() => {
-      this.email = 'mailto:&#x63;&#x68;&#x61;&#x6E;&#x67;&#x2E;&#x65;&#x64;&#x67;&#x61;&#x72;&#x31;&#x34;&#x37;&#x36;&#x40;&#x67;&#x6D;&#x61;&#x69;&#x6C;&#x2E;&#x63;&#x6F;&#x6D;';
+
+      // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+      // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+      // This code is freeware provided these four comment lines remain intact
+      // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+
+      const coded = "t8YwB.dDBYAjWJ5@BgY3z.tmg"
+      const key = "A7EhT5OSmeVcvz3ZLY41QqRFHyJn80kCsgdItrbuNK6WjofMXGliw2aUpxBD9P"
+      const shift = coded.length
+      let link = ""
+
+      for (let i = 0; i < coded.length; i++) {
+
+        if (key.indexOf(coded.charAt(i)) == -1) {
+
+          let ltr = coded.charAt(i)
+          link += (ltr)
+
+        } else {
+
+          let ltr = (key.indexOf(coded.charAt(i)) - shift + key.length) % key.length
+          link += (key.charAt(ltr))
+
+        }
+      }
+
+      this.email = 'mailto:' + link;
+
     }, 1000);
+
+    // setTimeout(() => {
+    //   this.email = 'mailto:&#099;&#104;&#097;&#110;&#103;&#046;&#101;&#100;&#103;&#097;&#114;&#049;&#052;&#055;&#054;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;';
+    // }, 1000);
 
   }
 
